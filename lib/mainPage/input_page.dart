af2a9ced7046a46081ff7564341fc1eea9d3919e
Simran Srivastava <52354644+simransrivastava01@users.noreply.github.com>
2020-07-11 22:45:29 +0530

    Update input_page.dart

--- lib/mainPage/input_page.dart
+++ lib/mainPage/input_page.dart
@@ -27,7 +27,7 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('New Hacks'),
+        title: Text('LinkUs'),
         centerTitle: true,
       ),
       // backgroundColor: Colors.red,
